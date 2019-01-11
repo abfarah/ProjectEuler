d30ecc639e811505a794bbf38479c5f8fc8d38e6
Abdullahif14688 <abdullahi.farah96@gmail.com>
2019-01-11 13:11:22 -0600

    Created a more efficient solution to the problem

--- Java/Problem1.java
+++ Java/Problem1.java
@@ -8,17 +8,32 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 */
 
-
 public class Problem1 {
 	public static void main(String[] args){
 		Problem1 test = new Problem1();
-		System.out.println(test.solve());
+		long startTime1 = System.nanoTime();
+		test.solve();
+		long endTime1 = System.nanoTime();
+		long duration1 = (endTime1 - startTime1);
+
+		Problem1 test2 = new Problem1();
+		long startTime2 = System.nanoTime();
+		test.solve2();
+		long endTime2 = System.nanoTime();
+		long duration2 = (endTime2 - startTime2);
+
+		System.out.println("Solution = " + test.solve2());
+		
+		System.out.println("Brute force solution took this long: " + duration1 + " nanoSeconds");
+		System.out.println("Second solution took this long: " + duration2 + " nanoSeconds");
+
+
 	}
 	
 	public String solve(){
 		int numb = 1000;
 		int sumOfMultiples = 0;
-		// First step find all multiples of 3 and 5 or both below 1000
+		// First step find all multiples of 3 and 5 or both below 1000.
 		for (int i = 0; i < numb; i++){
 			if((i%3) == 0 || (i%5) == 0){
 				sumOfMultiples += i;
@@ -26,6 +41,22 @@ public class Problem1 {
 		}
 
 		return Integer.toString(sumOfMultiples);
+	}
+
+	public String solve2(){
+		int numb = 999;
+
+		// Returning the sum of the multiples of 3 and 5 minus the double counted sums of the multiples of 15
+		int result = (findSumOfDivisors(numb, 3) + findSumOfDivisors(numb, 5)) - findSumOfDivisors(numb, 15);
+		return Integer.toString(result);
+	}
+
+	// This method is more efficient then brute force alternative.
+	// The first step is to find largest multiple
+	// The second step is to use the equation for sum and multiply by the divisor
+	public int findSumOfDivisors(int numb, int divisor){
+		int largestMultiple = (int) numb / divisor;
+		return (int) divisor * (largestMultiple * (largestMultiple + 1)) / 2;
 	}
 
 
