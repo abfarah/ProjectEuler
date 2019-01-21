b17db4f6cda8db443df679dba499f7a8312f931e
Abdullahif14688 <abdullahi.farah96@gmail.com>
2019-01-21 16:52:25 -0600

    updated problem 4 with a more efficient solution

--- Java/Problem4.java
+++ Java/Problem4.java
@@ -10,8 +10,22 @@ import java.lang.Math;
 
 public class Problem4 {
 	public static void main(String[] args){
-	    Problem4 test = new Problem4();
-		System.out.println(test.solve());
+		Problem4 test = new Problem4();
+		long startTime1 = System.nanoTime();
+		test.solve();
+		long endTime1 = System.nanoTime();
+		long duration1 = (endTime1 - startTime1);
+
+		Problem1 test2 = new Problem1();
+		long startTime2 = System.nanoTime();
+		test.solve2();
+		long endTime2 = System.nanoTime();
+		long duration2 = (endTime2 - startTime2);
+
+		System.out.println("Solution = " + test.solve2());
+		
+		System.out.println("Brute force solution took this long: " + duration1 + " nanoSeconds");
+		System.out.println("Second solution took this long: " + duration2 + " nanoSeconds");
 		
 	}
 	
@@ -42,5 +56,26 @@ public class Problem4 {
         } else {
             return false;
         }
+	}
+
+	public String solve2(){
+		int numb = 999;
+		int count;
+		int palindrome = 0;
+		
+		while (numb >= 100) {
+			count = 999;
+			while (count >= numb) {
+				if (count * numb <= palindrome){
+					break;
+				} else if (isPalindrome(Integer.toString(count * numb))) {
+					palindrome = count * numb;
+				}
+				count --;
+			}
+			numb--;
+		}
+
+		return Integer.toString(palindrome);
 	}
 }
