983a831be9bcc9daa630217e72d1dd16b9d8c8b8
Abdullahif14688 <abdullahi.farah96@gmail.com>
2019-01-25 20:19:08 -0600

    Created new function for more efficient way of solving problem

--- Java/Problem5.java
+++ Java/Problem5.java
@@ -9,7 +9,21 @@ What is the smallest positive number that is evenly divisible by all of the numb
 public class Problem5 {
 	public static void main(String[] args){
 		Problem5 test = new Problem5();
-		System.out.println(test.solve());
+		long startTime1 = System.nanoTime();
+		test.solve();
+		long endTime1 = System.nanoTime();
+		long duration1 = (endTime1 - startTime1);
+
+		Problem1 test2 = new Problem1();
+		long startTime2 = System.nanoTime();
+		test.solve2();
+		long endTime2 = System.nanoTime();
+		long duration2 = (endTime2 - startTime2);
+
+		System.out.println("Solution = " + test.solve2());
+		
+		System.out.println("Brute force solution took this long: " + duration1 + " nanoSeconds");
+		System.out.println("Second solution took this long: " + duration2 + " nanoSeconds");
 	}
 
 	public String solve(){
@@ -33,5 +47,9 @@ public class Problem5 {
 		}
 
 		return result;
+	}
+
+	public String solve2(){
+		return "";
 	}
 }
