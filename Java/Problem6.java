8d498b6217a44e1fab4074c3791cde7234de8d9a
Abdullahif14688 <abdullahi.farah96@gmail.com>
2019-02-15 22:20:19 -0600

    Refactored code with faster solution in method solve2

--- Java/Problem6.java
+++ Java/Problem6.java
@@ -17,7 +17,20 @@ import java.lang.Math;
 public class Problem6 {
 	public static void main(String[] args){
 		Problem6 test = new Problem6();
-		System.out.println(test.solve());
+		long startTime1 = System.nanoTime();
+		test.solve();
+		long endTime1 = System.nanoTime();
+		long duration1 = (endTime1 - startTime1);
+
+		long startTime2 = System.nanoTime();
+		test.solve2();
+		long endTime2 = System.nanoTime();
+		long duration2 = (endTime2 - startTime2);
+
+		System.out.println("Solution = " + test.solve2());
+		
+		System.out.println("Brute force solution took this long: " + duration1 + " nanoSeconds");
+		System.out.println("Second solution took this long: " + duration2 + " nanoSeconds");
 	}
 
 	public String solve(){
@@ -30,6 +43,13 @@ public class Problem6 {
 			squareOfSum += i;
 
 		}
+		return Integer.toString((squareOfSum * squareOfSum) - sumOfSquares);
+	}
+
+	public String solve2(){
+		int numb = 100;
+		int sumOfSquares = (((2*numb +1)*(numb +1)*numb)/6);
+		int squareOfSum = (numb*(numb +1)/2);
 		return Integer.toString((squareOfSum * squareOfSum) - sumOfSquares);
 	}
 }
