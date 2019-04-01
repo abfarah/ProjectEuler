2472b97ff9315b1fa2ee92bfb1e00c9140d597a2
abfarah <abdullahi.farah96@gmail.com>
2019-04-01 07:37:39 -0500

    Updated problem5 solution with more efficient algorithm

--- Java/Problem5.java
+++ Java/Problem5.java
@@ -5,11 +5,12 @@ Problem 5:
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
+import java.lang.Math;
 
 public class Problem5 {
 	public static void main(String[] args){
 		Problem5 test = new Problem5();
-		/*long startTime1 = System.nanoTime();
+		long startTime1 = System.nanoTime();
 		test.solve();
 		long endTime1 = System.nanoTime();
 		long duration1 = (endTime1 - startTime1);
@@ -17,12 +18,12 @@ public class Problem5 {
 		long startTime2 = System.nanoTime();
 		test.solve2();
 		long endTime2 = System.nanoTime();
-		long duration2 = (endTime2 - startTime2);*/
+		long duration2 = (endTime2 - startTime2);
 
-		System.out.println("Solution = " + test.solve());
+		System.out.println("Solution = " + test.solve2());
 		
-		//System.out.println("Brute force solution took this long: " + duration1 + " nanoSeconds");
-		//System.out.println("Second solution took this long: " + duration2 + " nanoSeconds");
+		System.out.println("Brute force solution took this long: " + duration1 + " nanoSeconds");
+		System.out.println("Second solution took this long: " + duration2 + " nanoSeconds");
 	}
 
 	public String solve(){
@@ -51,15 +52,25 @@ public class Problem5 {
 	public String solve2(){
 		int[] primeFactors = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 		int numbDivisiblyBy = 20;
-		int count = 1;
+		int count = 0;
 		int result = 1;
 		int exponent = 1;
+		double limit = Math.sqrt(numbDivisiblyBy);
 		boolean isdivisible = true;
 
 		while(primeFactors[count] <= numbDivisiblyBy){
 			exponent = 1;
+			if(isdivisible){
+				if(primeFactors[count] <= limit){
+					exponent = (int) (Math.log(numbDivisiblyBy)/Math.log(primeFactors[count]));
+				} else {
+					isdivisible = false;
+				}
+			}
+			result = result * (int) (Math.pow(primeFactors[count], exponent));
+			count++;
 		}
 
-		return "fail";
+		return Integer.toString(result);
 	}
 }
