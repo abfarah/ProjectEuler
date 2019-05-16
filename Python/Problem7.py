51c32fbc144d508907923ee59956b7db556fa10c
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-05-16 18:06:03 -0500

    Completed problem 7 in python

---
+++ Python/Problem7.py
@@ -0,0 +1,20 @@
+# By Abdullahi Farah
+# Problem 7:
+# 
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# 
+# What is the 10,001st prime number?
+
+import Common
+
+def solve(n):
+	result = 1
+	count = 1
+	while count != n:
+		result += 2
+		if Common.isPrime(result):
+			count += 1
+	return result
+
+print solve(10001)
+
